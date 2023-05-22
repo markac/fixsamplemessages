ba30afc4c320e8065d3dec8227c28d3b35028ddb
Mark Crowther <mark.crowther@weareadaptive.com>
2023-05-22 17:18:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='fixsamplemessages',
-    version='1.0',
+    version='1.0.1',
     py_modules=['fixsamplemessages'],
     install_requires = ['markdown>=3.0'],
 )
